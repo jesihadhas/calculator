42906f91d611e43497d9bae1544a5553d59ece25
jesihadhas <140829414+jesihadhas@users.noreply.github.com>
2023-07-28 21:59:00 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,23 +1,23 @@
-print("calculator")
+print("CALCULATOR")
 
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
 
 print("press 1 for addition")
-print("prtess 2 for substraction")
+print("press 2 for substraction")
 print("press 3 for multiplication")
 print("press 4 for division")
 
-operator=int(input("enter operators from1-4:"))
+operator=int(input("enter operators:"))
 
 if operator==1:
-    print(num1+num2)
+    print(("Answer="),num1+num2)
 elif operator==2:
-    print(num1-num2)
+    print(("Answer="),num1-num2)
 elif operator==3:
-    print(num1*num2)
+    print(("Answer="),num1*num2)
 elif operator==4:
-    print(num1/num2)
+    print(("Answer="),num1/num2)
 else:
     print("invalid input")
         
